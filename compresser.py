c7d73ff524144920504cb83be472680bd5141c1b
Samuel MICHEL-CHAGNOT <samuel.michel-chagnot@bordeaux-inp.fr>
2026-03-09 22:53:01 +0100

    fir filters

--- compresser.py
+++ compresser.py
@@ -1,11 +1,8 @@
 import os
 from PIL import Image
 
-# Dossier à parcourir
 img_dir = "img/preview"
-# Taille max en Ko (par exemple 500 Ko)
 MAX_SIZE_KO = 500
-# Largeur max en pixels (optionnel)
 MAX_WIDTH = 1920
 
 for root, dirs, files in os.walk(img_dir):
@@ -16,10 +13,8 @@ for root, dirs, files in os.walk(img_dir):
             if size_ko > MAX_SIZE_KO:
                 print(f"Compression de {path} ({size_ko} Ko)")
                 img = Image.open(path)
-                # Redimensionne si trop large
                 if img.width > MAX_WIDTH:
                     ratio = MAX_WIDTH / img.width
                     new_size = (MAX_WIDTH, int(img.height * ratio))
                     img = img.resize(new_size, Image.LANCZOS)
-                # Sauvegarde en JPEG compressé (qualité 85)
                 img.save(path, optimize=True, quality=85)
